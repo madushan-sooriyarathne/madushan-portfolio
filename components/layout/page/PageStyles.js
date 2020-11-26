1fa5734afead72bb15aac4bb4bbd2f444017b5b7
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-11-27 02:28:33 +0530

    minor layout changes in page component

--- components/layout/page/PageStyles.js
+++ components/layout/page/PageStyles.js
@@ -3,8 +3,10 @@ import styled from "styled-components";
 const PageWrapper = styled.div`
   display: grid;
   grid-template-columns: ${(props) =>
-    `[full-start] minmax(calc(${props.theme.navBarFoldedWidth} + 5rem), 1fr) [content-start] repeat(8, [col-start] minmax(min-content, 15rem) [col-end]) [content-end] minmax(5rem, 1fr) [full-end] `};
+    `[full-start] minmax(calc(${props.theme.navBarFoldedWidth} + 5rem), 1fr) [content-start] repeat(8, [col-start] minmax(min-content, 18rem) [col-end]) [content-end] minmax(5rem, 1fr) [full-end] `};
   grid-auto-rows: max-content;
+  width: 100%;
+  min-height: 100vh;
 `;
 
 export default PageWrapper;
