bf2f8cf19338a6d3a254422b4b37ecf371a158e6
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-11-29 03:02:49 +0530

    created a data file to hold minor content

---
+++ data/data.js
@@ -0,0 +1,32 @@
+const socialLinks = [
+  {
+    name: "linkedin",
+    url: "https://www.linkedin.com/in/madushan-sooriyarathne/",
+    id: 0,
+  },
+  { name: "github", url: "https://github.com/madushan-sooriyarathne", id: 1 },
+  { name: "instagram", url: "https://www.instagram.com/iam_madushan/", id: 2 },
+  {
+    name: "facebook",
+    url: "https://www.facebook.com/madushan.sooriyarathne/",
+    id: 3,
+  },
+];
+
+const quickLinks = [
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/madushan-sooriyarathne/",
+    id: 0,
+  },
+  { name: "GitHub", url: "https://github.com/madushan-sooriyarathne", id: 1 },
+  { name: "Instagram", url: "https://www.instagram.com/iam_madushan/", id: 2 },
+  {
+    name: "Facebook",
+    url: "https://www.facebook.com/madushan.sooriyarathne/",
+    id: 3,
+  },
+  { name: "Advertaro Creative Agency", url: "https://advertaro.lk/", id: 4 },
+];
+
+export { socialLinks, quickLinks };
