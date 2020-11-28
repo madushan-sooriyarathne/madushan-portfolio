56572601073fe88873e0b5368da45843bba60193
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-11-28 06:58:49 +0530

    created the about page

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import AboutSection from "../components/layout/about/AboutSection";
+import Page from "../components/layout/page/Page";
+
+const About = () => {
+  return (
+    <Page>
+      <AboutSection />
+    </Page>
+  );
+};
+
+export default About;
