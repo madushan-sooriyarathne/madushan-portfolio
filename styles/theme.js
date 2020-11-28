920fd062e28bdf1e395ec00d8bc6ffcf3ab95195
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-11-28 06:57:51 +0530

    added some theme data to theme object

--- styles/theme.js
+++ styles/theme.js
@@ -2,9 +2,11 @@ const theme = {
   colorBlack: "#000",
   colorBlackLight: "#1a1a1a",
   colorWhite: "#fff",
-  navBarFoldedWidth: "15rem",
   fontPrimary: "'Avenir Next', sans-serif",
   fontSecondary: "'Almondita Regular','Times New Roman', Times, serif",
+  navBarFoldedWidth: "15rem",
+  headerHeight: "10rem",
+  footerHeight: "10rem",
 };
 
 export default theme;
