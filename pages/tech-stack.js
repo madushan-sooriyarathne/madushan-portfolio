a6ecd5badd1a71dfb4ccf86e815ee09e1bc835d3
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-11-30 21:18:05 +0530

    added the TechStackSection to tech-stack.js

--- pages/tech-stack.js
+++ pages/tech-stack.js
@@ -1,7 +1,12 @@
 import Page from "../components/layout/page/Page";
+import TechStackSection from "../components/layout/tech-stack-section/TechStackSection";
 
 const TechStack = () => {
-  return <Page></Page>;
+  return (
+    <Page>
+      <TechStackSection />
+    </Page>
+  );
 };
 
 export default TechStack;
