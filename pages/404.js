46cf1b73e5c278aa6dbac6ba3868583a27e5f6d7
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-11-30 21:17:00 +0530

    created the custom 404 page and added the CustomErrorComponent

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Page from "../components/layout/page/Page";
+import CustomErrorComponent from "../components/layout/custom-error-component/CustomErrorComponent";
+
+const Custom404Error = () => {
+  return (
+    <Page>
+      <CustomErrorComponent />
+    </Page>
+  );
+};
+
+export default Custom404Error;
