58615d0cc3657f21519de850fcfd5d913b65bec0
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-01 03:31:55 +0530

    added colorGray and colorGrayLight

--- styles/theme.js
+++ styles/theme.js
@@ -2,6 +2,8 @@ const theme = {
   colorBlack: "#000",
   colorBlackLight: "#1a1a1a",
   colorWhite: "#fff",
+  colorGray: "#636e72",
+  colorGrayLight: "#b2bec3",
   fontPrimary: "'Avenir Next', sans-serif",
   fontSecondary: "'Almondita Regular','Times New Roman', Times, serif",
   navBarFoldedWidth: "15rem",
