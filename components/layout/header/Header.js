40e223210f1f547bca98b0afa5dac5d626837be5
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-01 00:52:42 +0530

    addded a slide in navBar to Header Component

--- components/layout/header/Header.js
+++ components/layout/header/Header.js
@@ -1,13 +1,97 @@
+import { useState } from "react";
 import Link from "next/link";
-import { HeaderWrapper, Logo } from "./HeaderStyles";
+import { useRouter } from "next/router";
+import { useSpring, useTrail } from "react-spring";
+
+import { navBarLinks } from "../../../data/data";
+
+import {
+  HeaderWrapper,
+  Logo,
+  MenuButton,
+  SVGWrapper,
+  LineOne,
+  LineThree,
+  LineTwo,
+  NavBar,
+  NavButtonGroup,
+  NavLinkWrapper,
+  NavBarItem,
+} from "./HeaderStyles";
 
 const Header = () => {
+  const router = useRouter();
+  const [open, setOpen] = useState(false);
+  const navBarProps = useSpring({
+    transform: open ? `translateX(0)` : `translateX(-100%)`,
+    duration: 500,
+  });
+
+  const trail = useTrail(navBarLinks.length, {
+    config: { mass: 5, tension: 2000, friction: 200 },
+    opacity: open ? 1 : 0,
+    x: open ? 0 : 20,
+    height: open ? 110 : 0,
+    from: { opacity: 0, x: 20, height: 0 },
+    delay: 500,
+  });
+
+  const handleMenuOpen = (event) => {
+    setOpen((prvState) => !prvState);
+  };
+
+  const handleMenuClose = (event) => {
+    setOpen(false);
+  };
+
+  const handleRouteChange = (route) => {
+    setTimeout(() => {
+      router.push(route);
+    }, 500);
+    setOpen(false);
+  };
+
   return (
-    <HeaderWrapper>
-      <Link href="/">
-        <Logo>Madushan Sooriyarathne</Logo>
-      </Link>
-    </HeaderWrapper>
+    <>
+      <HeaderWrapper>
+        <Link href="/">
+          <Logo>Madushan Sooriyarathne</Logo>
+        </Link>
+        <MenuButton onClick={handleMenuOpen}>
+          <SVGWrapper width="50" height="50" viewBox="0 0 100 100">
+            <LineOne
+              open={open}
+              d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
+            />
+            <LineTwo open={open} d="M 20,50 H 80" />
+            <LineThree
+              open={open}
+              d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
+            />
+          </SVGWrapper>
+        </MenuButton>
+      </HeaderWrapper>
+      <NavBar
+        style={{ transform: navBarProps.transform.interpolate((x) => x) }}
+      >
+        <NavButtonGroup>
+          {trail.map(({ x, height, ...rest }, index) => (
+            <NavLinkWrapper
+              onClick={() => handleRouteChange(navBarLinks[index].route)}
+              key={index}
+              style={{
+                ...rest,
+                transform: x.interpolate((x) => `translate3d(0, ${x}px, 0)`),
+              }}
+            >
+              <NavBarItem styled={{ height }}>
+                {navBarLinks[index].name}
+              </NavBarItem>
+            </NavLinkWrapper>
+          ))}
+        </NavButtonGroup>
+      </NavBar>
+    </>
   );
 };
 
--- components/layout/header/HeaderStyles.js
+++ components/layout/header/HeaderStyles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { animated } from "react-spring";
 
 const HeaderWrapper = styled.header`
   position: absolute;
@@ -9,9 +10,10 @@ const HeaderWrapper = styled.header`
   height: auto;
   display: flex;
   align-items: center;
-  justify-content: center;
-  padding: 2rem;
+  justify-content: space-between;
+  padding: 2rem 10rem;
   background-color: transparent;
+  z-index: 20;
 `;
 
 const Logo = styled.p`
@@ -25,4 +27,94 @@ const Logo = styled.p`
   }
 `;
 
-export { HeaderWrapper, Logo };
+const MenuButton = styled.button`
+  background-color: transparent;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  display: flex;
+  padding: 0;
+`;
+
+const SVGWrapper = styled.svg`
+  & > * {
+    fill: none;
+    stroke: ${(props) => props.theme.colorWhite};
+    stroke-width: 3;
+    transition: stroke-dasharray 0.5s cubic-bezier(0.4, 0, 0.2, 1),
+      stroke-dashoffset 0.5s cubic-bezier(0.4, 0, 0.2, 1);
+  }
+`;
+
+const LineOne = styled.path`
+  stroke-dasharray: ${(props) => (props.open ? "90 207" : "60 207")};
+  stroke-dashoffset: ${(props) => props.open && -134};
+  stroke-width: 3;
+`;
+
+const LineTwo = styled.path`
+  stroke-dasharray: ${(props) => (props.open ? "1 60" : "60 60")};
+  stroke-dashoffset: ${(props) => props.open && -30};
+  stroke-width: 3;
+`;
+
+const LineThree = styled.path`
+  stroke-dasharray: ${(props) => (props.open ? "90 207" : "60 207")};
+  stroke-dashoffset: ${(props) => props.open && -134};
+  stroke-width: 3;
+`;
+
+const NavBar = styled(animated.div)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: ${(props) => props.theme.colorBlackLight};
+  z-index: 10;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const NavButtonGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const NavLinkWrapper = styled(animated.div)`
+  padding: 2rem;
+`;
+
+const NavBarItem = styled(animated.a)`
+  font-size: 5rem;
+  font-weight: 900;
+  color: ${(props) => props.theme.colorWhite};
+  opacity: 0.5;
+  letter-spacing: 2px;
+  cursor: pointer;
+
+  transition: opacity 0.3s ease-in;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+export {
+  HeaderWrapper,
+  Logo,
+  MenuButton,
+  SVGWrapper,
+  LineOne,
+  LineTwo,
+  LineThree,
+  NavBar,
+  NavButtonGroup,
+  NavLinkWrapper,
+  NavBarItem,
+};
