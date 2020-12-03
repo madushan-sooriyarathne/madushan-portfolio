e80b5efc171231df4e8e2899a8b3b05c102aa88d
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-04 00:05:11 +0530

    added centered opiton to title-gropu

--- components/layout/title-group/TitleGroupStyles.js
+++ components/layout/title-group/TitleGroupStyles.js
@@ -3,7 +3,8 @@ import styled from "styled-components";
 const Group = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
+  align-items: ${(props) => (props.centered ? "center" : "flex-start")};
+  text-align: ${(props) => (props.centered ? "center" : "left")};
 `;
 
 export { Group };
