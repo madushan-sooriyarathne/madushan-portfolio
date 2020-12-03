8129e681150d7c4acdaf2f3ef467fd5e36562c4f
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-04 03:00:55 +0530

    crated the page route

---
+++ pages/work.js
@@ -0,0 +1,12 @@
+import Page from "../components/layout/page/Page";
+import WorkSection from "../components/layout/work-section/WorkSection";
+
+const Work = () => {
+  return (
+    <Page>
+      <WorkSection />
+    </Page>
+  );
+};
+
+export default Work;
