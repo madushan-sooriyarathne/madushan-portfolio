5a5feddf97a1de334837e8c033de55e9bdae5ddd
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-04 02:15:37 +0530

    finished TechStackSection

--- components/layout/tech-stack-section/TechStackSection.js
+++ components/layout/tech-stack-section/TechStackSection.js
@@ -2,18 +2,84 @@ import TitleGroup from "../title-group/TitleGroup";
 import HeadingPrimary from "../../headings/heading-primary/HeadingPrimary";
 import HeadingSub from "../../headings/heading-sub/HeadingSub";
 
-import { Section, ContentWrapper } from "./TechStackSectionStyles";
+import {
+  Section,
+  ContentWrapper,
+  StackGroup,
+  StackHeading,
+  TechItem,
+  TechItemLogo,
+  TechItemName,
+  ItemsWrapper,
+} from "./TechStackSectionStyles";
+
+const techStack = [
+  {
+    name: "Frontend Development",
+    stackId: 0,
+    items: [
+      { name: "Javascript", logo: "javascript.svg", id: 0 },
+      { name: "React", logo: "react.svg", id: 1 },
+      { name: "Next.js", logo: "next-js.svg", id: 2 },
+      { name: "Redux", logo: "redux.svg", id: 3 },
+      { name: "Sass", logo: "sass.svg", id: 4 },
+    ],
+  },
+  {
+    name: "Backend Development",
+    stackId: 1,
+    items: [
+      { name: "Node.js", logo: "nodejs.svg", id: 0 },
+      { name: "Express", logo: "express.svg", id: 1 },
+      { name: "Python", logo: "python.svg", id: 2 },
+      { name: "Flask", logo: "flask.svg", id: 3 },
+    ],
+  },
+  {
+    name: "Database Solutions",
+    stackId: 0,
+    items: [
+      { name: "MongoDB", logo: "mongodb.svg", id: 0 },
+      { name: "PostgrSQL", logo: "postgresql.svg", id: 1 },
+    ],
+  },
+  {
+    name: "Creative Design",
+    stackId: 0,
+    items: [
+      { name: "Figma", logo: "figma.svg", id: 0 },
+      { name: "Adobe Illustrator", logo: "ai.svg", id: 1 },
+      { name: "Adobe Photoshop", logo: "ps.svg", id: 2 },
+    ],
+  },
+];
 
 const TechStackSection = () => {
   return (
     <Section>
+      <TitleGroup>
+        <HeadingPrimary>What I use</HeadingPrimary>
+        <HeadingSub>
+          This is my current go-to stack. This can change time to time.
+        </HeadingSub>
+      </TitleGroup>
       <ContentWrapper>
-        <TitleGroup>
-          <HeadingPrimary>What I use</HeadingPrimary>
-          <HeadingSub>
-            This is my current go-to stack. This can change time to time.
-          </HeadingSub>
-        </TitleGroup>
+        {techStack.map((stack) => (
+          <StackGroup key={stack.stackId}>
+            <StackHeading>{stack.name}</StackHeading>
+            <ItemsWrapper>
+              {stack.items.map((item) => (
+                <TechItem key={item.id}>
+                  <TechItemLogo
+                    src={`/static/svg/${item.logo}`}
+                    alt={item.name}
+                  />
+                  <TechItemName>{item.name}</TechItemName>
+                </TechItem>
+              ))}
+            </ItemsWrapper>
+          </StackGroup>
+        ))}
       </ContentWrapper>
     </Section>
   );
--- components/layout/tech-stack-section/TechStackSectionStyles.js
+++ components/layout/tech-stack-section/TechStackSectionStyles.js
@@ -3,27 +3,114 @@ import styled from "styled-components";
 const Section = styled.section`
   grid-column: content-start / content-end;
   display: grid;
-  grid-template-columns: repeat(
-    2,
-    [col-start] minmax(min-content, 1fr) [col-end]
-  );
-  grid-template-areas: "ts im";
+  grid-template-columns: 1fr;
+  grid-auto-rows: max-content;
   gap: 5rem;
-  align-items: start;
-  justify-items: start;
-  padding-top: ${(props) => props.theme.headerHeight};
+  align-items: center;
+  justify-items: center;
+  padding: 10rem;
+  padding-top: ${(props) => `calc(${props.theme.headerHeight} + 5rem)`};
   min-height: ${(props) => `calc(100vh - ${props.theme.footerHeight}) `};
 `;
 
 const ContentWrapper = styled.div`
-  grid-area: ts;
-
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: flex-start;
+  display: grid;
+  grid-template-columns: repeat(
+    2,
+    [col-start] minmax(min-content, 1fr) [col-end]
+  );
+  gap: 5rem;
+  align-items: start;
+  justify-items: start;
 
   padding: 5rem;
 `;
 
-export { Section, ContentWrapper };
+const StackGroup = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-auto-rows: max-content;
+  gap: 2rem;
+  align-items: center;
+  justify-items: center;
+  width: 100%;
+  height: 100%;
+  padding: 3rem;
+
+  border-radius: 5px;
+  background-color: ${(props) => props.theme.colorBlackLight};
+
+  transition: transform 0.3s ease-in-out;
+
+  &:hover {
+    transform: translateY(-3px);
+  }
+`;
+
+const StackHeading = styled.h2`
+  font-size: 2.4rem;
+  font-weight: 700;
+  text-align: center;
+
+  align-self: center;
+`;
+
+const ItemsWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  flex-wrap: wrap;
+  width: 100%;
+  height: 100%;
+`;
+
+const TechItem = styled.div`
+  display: grid;
+  grid-template-columns: min-content minmax(min-content, 1fr);
+  align-items: center;
+  justify-items: start;
+
+  background-color: transparent;
+  border: ${(props) => `1px solid ${props.theme.colorWhite}`};
+  border-radius: 5px;
+  margin-bottom: 2rem;
+  margin-right: 2rem;
+  transition: background-color 0.2s ease-in, border 0.2s ease-in;
+
+  &:hover {
+    background-color: ${(props) => props.theme.colorWhite};
+    border: ${(props) => `1px solid ${props.theme.colorBlack}`};
+
+    & ${TechItemName} {
+      color: ${(props) => props.theme.colorBlack};
+    }
+  }
+`;
+
+const TechItemLogo = styled.img`
+  width: 4rem;
+  height: 4rem;
+  padding: 0.5rem;
+`;
+
+const TechItemName = styled.p`
+  font-size: 1.5rem;
+  padding: 1rem;
+  font-weight: 600;
+  color: inherit;
+  letter-spacing: 1px;
+  line-height: 1;
+
+  transition: color 0.2s ease-in;
+`;
+
+export {
+  Section,
+  ContentWrapper,
+  StackGroup,
+  StackHeading,
+  ItemsWrapper,
+  TechItem,
+  TechItemLogo,
+  TechItemName,
+};
