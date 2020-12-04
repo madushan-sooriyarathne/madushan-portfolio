e2ff2e5a6ba67991d89c13a41ff22f8c3d3fff64
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-05 03:17:12 +0530

    added the WorkItem Component to WorkSection

--- components/layout/work-section/WorkSection.js
+++ components/layout/work-section/WorkSection.js
@@ -1,5 +1,26 @@
+import TitleGroup from "../title-group/TitleGroup";
+import HeadingPrimary from "../../headings/heading-primary/HeadingPrimary";
+import HeadingSub from "../../headings/heading-sub/HeadingSub";
+
+import { Section, WorkGroup } from "./WorkSectionStyles";
+import WorkItem from "../../work-item/WorkItem";
+
+import { workItems } from "../../../data/data";
+
 const WorkSection = () => {
-  return <>Work</>;
+  console.log(workItems);
+  return (
+    <Section>
+      <TitleGroup centered>
+        <HeadingPrimary>My Recent Work.</HeadingPrimary>
+      </TitleGroup>
+      <WorkGroup>
+        {workItems.map((item) => (
+          <WorkItem workItem={item} key={item.id} />
+        ))}
+      </WorkGroup>
+    </Section>
+  );
 };
 
 export default WorkSection;
---
+++ components/layout/work-section/WorkSectionStyles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+const Section = styled.div`
+  grid-column: content-start / content-end;
+
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-auto-rows: max-content;
+  gap: 5rem;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 100%;
+`;
+
+const WorkGroup = styled.div`
+  display: grid;
+  grid-template-columns: repeat(
+    3,
+    [col-start] minmax(min-content, 1fr) [col-end]
+  );
+  gap: 3rem;
+  padding: 5rem 0;
+`;
+
+export { Section, WorkGroup };
