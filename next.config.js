e8109705ca3849d7411eb1eebacc59b274e37058
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-07 02:39:57 +0530

    added enviornment varibales for contentful API keys

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  env: {},
+  env: {
+    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+    CONTENTFUL_DELIVERY_API_KEY: process.env.CONTENTFUL_DELIVERY_API_KEY,
+    CONTENTFUL_PREVIEW_API_KEY: process.env.CONTENTFUL_PREVIEW_API_KEY,
+  },
 };
