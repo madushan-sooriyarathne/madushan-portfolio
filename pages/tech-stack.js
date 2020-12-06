1cac3e776d6420efbac20fc65ee007155acaee21
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-07 02:51:29 +0530

    added getStaticProps to fetch data on build

--- pages/tech-stack.js
+++ pages/tech-stack.js
@@ -1,12 +1,36 @@
+import { fetchEntries } from "../contentful/contentfulFetch";
+
 import Page from "../components/layout/page/Page";
 import TechStackSection from "../components/layout/tech-stack-section/TechStackSection";
 
-const TechStack = () => {
+const TechStack = ({ stacks }) => {
   return (
     <Page>
-      <TechStackSection />
+      <TechStackSection stacks={stacks} />
     </Page>
   );
 };
 
+const getStaticProps = async () => {
+  // fetch TechStacks from contentful
+  const res = await fetchEntries("techStack");
+  const stacks = res.map((entry) => {
+    const items = entry.fields.stackItems.map((item) => ({
+      name: item.fields.name,
+      logo: item.fields.logo.fields.file.url,
+      stackItemId: item.fields.stackItemId,
+    }));
+    return {
+      stackName: entry.fields.stackName,
+      stackId: entry.fields.stackId,
+      stackItems: items,
+    };
+  });
+
+  return {
+    props: { stacks },
+  };
+};
+
+export { getStaticProps };
 export default TechStack;
