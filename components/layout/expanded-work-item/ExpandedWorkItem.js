025c6e1a184e2cbbd442004e5dcb19b661caa65b
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-07 02:45:11 +0530

    created ExpandedWorkItem component

---
+++ components/layout/expanded-work-item/ExpandedWorkItem.js
@@ -0,0 +1,53 @@
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+
+import HeadingPrimary from "../../headings/heading-primary/HeadingPrimary";
+import Button from "../../button/Button";
+
+import {
+  Wrapper,
+  Image,
+  DetailsWrapper,
+  TitleWrapper,
+  WorkType,
+  TechStackGroup,
+  StatusBadge,
+  WorkDescription,
+  ButtonWrapper,
+} from "./ExpandedWorkItemStyles";
+import { useEffect } from "react";
+
+const ExpandedWorkItem = ({ workItem }) => {
+  const handleRedirect = () => {
+    window.open(workItem.url, "_blank");
+  };
+
+  useEffect(() => {
+    window.scroll(0, 0);
+  }, []);
+
+  return (
+    <Wrapper>
+      <Image src={workItem.image} />
+      <DetailsWrapper>
+        <TitleWrapper>
+          <HeadingPrimary>{workItem.name}</HeadingPrimary>
+          <WorkType>{workItem.type}</WorkType>
+        </TitleWrapper>
+        <TechStackGroup></TechStackGroup>
+        <StatusBadge status={workItem.status.toLowerCase().replace(" ", "-")}>
+          {workItem.status}
+        </StatusBadge>
+        <WorkDescription
+          dangerouslySetInnerHTML={{
+            __html: documentToHtmlString(workItem.description),
+          }}
+        />
+        <ButtonWrapper>
+          <Button onClick={handleRedirect}>Visit Project</Button>
+        </ButtonWrapper>
+      </DetailsWrapper>
+    </Wrapper>
+  );
+};
+
+export default ExpandedWorkItem;
---
+++ components/layout/expanded-work-item/ExpandedWorkItemStyles.js
@@ -0,0 +1,108 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  grid-column: content-start / content-end;
+  padding: 0 10rem;
+`;
+
+const Image = styled.img`
+  width: 100%;
+`;
+
+const DetailsWrapper = styled.div`
+  grid-column: content-start / content-end;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-auto-rows: max-content;
+  gap: 2rem;
+  align-items: start;
+  justify-items: start;
+  background-color: ${(props) => props.theme.colorBlackLight};
+
+  padding: 2rem;
+`;
+
+const TitleWrapper = styled.div`
+  grid-column: 1 / 2;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+`;
+
+const WorkType = styled.p`
+  font-size: 1.4rem;
+  font-weight: 600;
+  color: ${(props) => props.theme.colorGray};
+`;
+
+const TechStackGroup = styled.div`
+  grid-column: 2 / 3;
+  grid-row: 1 / 3;
+`;
+
+const StatusBadge = styled.p`
+  grid-column: 1 / 2;
+  grid-row: 2 /3;
+  font-size: 1.5rem;
+  font-weight: 600;
+  letter-spacing: 1px;
+  line-height: 1;
+  padding: 0.5rem 1rem;
+  border-radius: 3px;
+
+  color: ${(props) =>
+    props.status === "in-development"
+      ? props.theme.colorWarning
+      : props.status === "finished"
+      ? props.theme.colorSuccess
+      : props.theme.colorDanger};
+  background-color: ${(props) =>
+    `rgba(${
+      props.status === "in-development"
+        ? props.theme.colorWarningRGB
+        : props.status === "finished"
+        ? props.theme.colorSuccessRGB
+        : props.theme.colorDangerRGB
+    }, 0.3)`};
+`;
+
+const WorkDescription = styled.p`
+  grid-column: 1 / 3;
+  grid-row: 3 / 4;
+  font-size: 1.6rem;
+  font-weight: 400;
+  letter-spacing: 1px;
+  line-height: 1.2;
+
+  & a {
+    color: inherit;
+    outline: none;
+    text-decoration: none;
+    padding: 2px 3px;
+    border-bottom: 1px solid transparent;
+    background-color: rgba(255, 255, 255, 0.2);
+    transition: border-bottom 0.2s ease;
+
+    &:hover {
+      border-bottom: ${(props) => `1px solid ${props.theme.colorWhite}`};
+    }
+  }
+`;
+
+const ButtonWrapper = styled.p`
+  grid-column: 1 / 3;
+  grid-row: 4 / 5;
+`;
+
+export {
+  Wrapper,
+  Image,
+  DetailsWrapper,
+  TitleWrapper,
+  WorkType,
+  TechStackGroup,
+  StatusBadge,
+  WorkDescription,
+  ButtonWrapper,
+};
