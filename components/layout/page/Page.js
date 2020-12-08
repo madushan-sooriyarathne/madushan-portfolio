3be4ca76e489a61d79c6aa1fe013d0f15fed15c2
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-08 23:58:30 +0530

    removed title from Page component as site wide title is there in _app.js

--- components/layout/page/Page.js
+++ components/layout/page/Page.js
@@ -1,19 +1,12 @@
-import Head from "next/head";
 import PageOverlay from "../page-overlay/PageOverlay";
 import Footer from "../footer/Footer";
 import Header from "../header/Header";
 import { PageWrapper, ContentWrapper } from "./PageStyles";
 
-const Page = ({
-  children,
-  title = "madushan.dev | Full stack dev & creative designer",
-}) => {
+const Page = ({ children }) => {
   return (
     <PageWrapper>
       <PageOverlay />
-      <Head>
-        <title>{title}</title>
-      </Head>
       <Header />
       <ContentWrapper>{children}</ContentWrapper>
       <Footer />
