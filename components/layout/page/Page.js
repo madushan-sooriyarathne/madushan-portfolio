9c9b82d7e78ea5330ea076555c72c87f606bfa2d
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-10 11:48:04 +0530

    added floter to page

--- components/layout/page/Page.js
+++ components/layout/page/Page.js
@@ -2,6 +2,7 @@ import PageOverlay from "../page-overlay/PageOverlay";
 import Footer from "../footer/Footer";
 import Header from "../header/Header";
 import { PageWrapper, ContentWrapper } from "./PageStyles";
+import Floater from "../../floater/Floater";
 
 const Page = ({ children }) => {
   return (
@@ -10,6 +11,7 @@ const Page = ({ children }) => {
       <Header />
       <ContentWrapper>{children}</ContentWrapper>
       <Footer />
+      <Floater />
     </PageWrapper>
   );
 };
