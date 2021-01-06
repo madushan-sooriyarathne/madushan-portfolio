e7c790646da76c770392aaecb9f02eeafebea26b
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-01-06 23:18:56 +0530

    added copyright symbol to the footer

--- components/layout/footer/Footer.js
+++ components/layout/footer/Footer.js
@@ -3,7 +3,9 @@ import { FooterWrapper, Copyright } from "./FooterStyles";
 const Footer = () => {
   return (
     <FooterWrapper>
-      <Copyright>{new Date().getFullYear()} - Madushan Sooriyarathne</Copyright>
+      <Copyright>
+        © {new Date().getFullYear()} - Madushan Sooriyarathne
+      </Copyright>
       <Copyright>Handcrafted with ❤️ by me</Copyright>
     </FooterWrapper>
   );
