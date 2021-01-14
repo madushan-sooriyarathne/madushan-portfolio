b79de56425964548a631337f1977259ceae62f3e
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-01-15 02:58:20 +0530

    added types for Contentful fetch results and next function's results

--- types.d.ts
+++ types.d.ts
@@ -1,7 +1,81 @@
-type EmailDetails = {
-  from: string;
-  to: string;
-  subject: string;
-  text?: string;
-  html?: string;
-};
+// Just to get document.fonts.ready types working. 😕
+declare global {
+  const FontFace: FontFace;
+
+  interface Document {
+    fonts: FontFaceSet;
+  }
+
+  type EmailDetails = {
+    from: string;
+    to: string;
+    subject: string;
+    text?: string;
+    html?: string;
+  };
+
+  interface ContentfulStackItemResult {
+    fields: {
+      name: string;
+      logo: { fields: { file: { url: string } } };
+      stackItemId: string;
+    };
+  }
+
+  interface StackItem {
+    name: string;
+    logo: string;
+    stackItemId: string;
+  }
+
+  interface ContentfulTechStackResult {
+    stackName: string;
+    stackId: string;
+    stackItems: ContentfulStackItemResult[];
+  }
+
+  interface TechStack {
+    stackName: string;
+    stackId: string;
+    stackItems: StackItem[];
+  }
+
+  type CSSOMString = string;
+  type FontFaceLoadStatus = "unloaded" | "loading" | "loaded" | "error";
+  type FontFaceSetStatus = "loading" | "loaded";
+
+  interface FontFace extends FontFaceDescriptors {
+    new (
+      family: string,
+      source: string | ArrayBuffer,
+      descriptors?: FontFaceDescriptors
+    ): FontFace;
+    readonly status: FontFaceLoadStatus;
+    readonly loaded: Promise<FontFace>;
+    variationSettings: CSSOMString;
+    display: CSSOMString;
+    load(): Promise<FontFace>;
+  }
+
+  interface FontFaceDescriptors {
+    family: CSSOMString;
+    style: CSSOMString;
+    weight: CSSOMString;
+    stretch: CSSOMString;
+    unicodeRange: CSSOMString;
+    variant: CSSOMString;
+    featureSettings: CSSOMString;
+  }
+
+  interface FontFaceSet extends Iterable<FontFace> {
+    readonly status: FontFaceSetStatus;
+    readonly ready: Promise<FontFaceSet>;
+    add(font: FontFace): void;
+    check(font: string, text?: string): Boolean; // throws exception
+    load(font: string, text?: string): Promise<FontFace[]>;
+    delete(font: FontFace): void;
+    clear(): void;
+  }
+}
+
+export {};
