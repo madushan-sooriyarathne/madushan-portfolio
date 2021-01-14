c9d9f366beab5c371bd93419bed0a4bb6e7fed93
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-01-15 00:19:53 +0530

    Converted contentful helper functions to Typescript

---
+++ contentful/contentfulFetch.ts
@@ -0,0 +1,25 @@
+import { ContentfulClientApi, Entry, EntryCollection } from "contentful";
+
+// import and config contentful client
+const client: ContentfulClientApi = require("contentful").createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_DELIVERY_API_KEY,
+});
+
+const fetchEntries = async (contentType: string): Promise<Entry<unknown>[]> => {
+  const entries: EntryCollection<unknown> = await client.getEntries({
+    content_type: contentType,
+  });
+  if (entries.items) return entries.items;
+  console.error("error fetching entries from contentful");
+  return [];
+};
+
+const fetchEntryById = async (id: string): Promise<unknown> => {
+  const entry: Entry<unknown> = await client.getEntry(id);
+  if (entry.fields) return entry.fields;
+  console.error("error fetching the ase study for given id");
+  return {};
+};
+
+export { fetchEntries, fetchEntryById };
