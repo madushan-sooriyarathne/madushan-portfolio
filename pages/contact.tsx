f174ae0de9eea4e489d160ece3895e4a6080d7ff
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-01-15 02:01:09 +0530

    converted contact page to typescript

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,7 +1,7 @@
 import Page from "../components/layout/page/Page";
 import ContactSection from "../components/layout/contact-section/ContactSection";
 
-const Contact = () => {
+const Contact: React.FC = (): JSX.Element => {
   return (
     <Page>
       <ContactSection />
